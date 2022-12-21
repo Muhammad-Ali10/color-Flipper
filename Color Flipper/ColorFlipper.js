421adc335f86a53f5ae1d6816587da5d7f3d4294
Muhammad-Ali10 <95209696+Muhammad-Ali10@users.noreply.github.com>
2022-12-22 00:44:49 +0500

    Update ColorFlipper.js

--- Color Flipper/ColorFlipper.js
+++ Color Flipper/ColorFlipper.js
@@ -7,7 +7,7 @@ btn.addEventListener("click", ()=>
     
     const color_length = color_array.length;
     const arr_num = Math.floor(Math.random() * color_length);
-    console.log(arr_num)
+    //console.log(arr_num)
     document.body.style.backgroundColor = color_array[arr_num]
     color.textContent = color_array[arr_num]
 
